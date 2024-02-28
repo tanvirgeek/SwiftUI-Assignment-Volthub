11d512a99c9c3a7292197d8cbaa28874228139b3
tanvirgeek <tanvirgeek@gmail.com>
2024-02-28 14:45:02 +0600

    Initial Commit

---
+++ Volthub/VolthubApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolthubApp.swift
+//  Volthub
+//
+//  Created by Tanvir Alam on 28/2/24.
+//
+
+import SwiftUI
+
+@main
+struct VolthubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
